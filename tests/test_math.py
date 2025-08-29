4254aff1057a0139fb449a3d07396044cefd609c
Roman Lütkenhaus <roman.luetkenhaus@fh-muenster.de>
2025-08-30 00:19:45 +0200

    Add simple math tests (with one intentional fail)

---
+++ tests/test_math.py
@@ -0,0 +1,9 @@
+def test_addition():
+    assert 2 + 2 == 4
+
+def test_multiplication():
+    assert 3 * 3 == 9
+
+def test_fail_example():
+    # Dieser Test ist absichtlich falsch, damit du den roten Fehler siehst
+    assert 5 - 2 == 10
